392b2e1e8c33c7ed2a6cd5bb6983de86718e33e5
Jan Kreps <jan.kreps@cvut.cz>
2023-07-20 13:01:41 +0200

    zmeny

---
+++ awx/set-connection.sh
@@ -0,0 +1,9 @@
+# tohle se nespousti, ale sourcuje
+# spusteny script nastavi prostredi pro potomky, ale ne pro rodicovsky shell.
+
+export CONTROLLER_HOST=awx.ciirc.cvut.cz
+export CONTROLLER_USERNAME=admin
+export CONTROLLER_PASSWORD='admin'
+export CONTROLLER_VERIFY_SSL=false
+
+
